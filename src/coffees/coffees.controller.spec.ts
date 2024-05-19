0e2d142ad609ae0fe8fab2efe668439bae9c5017
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-05-19 12:13:43 -0300

    Add more HTTP routes examples and refactor tests

--- src/coffees/coffees.controller.spec.ts
+++ src/coffees/coffees.controller.spec.ts
@@ -12,7 +12,43 @@ describe('CoffeesController', () => {
     controller = module.get<CoffeesController>(CoffeesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findOne', () => {
+    it('should return the coffee with the specified id', () => {
+      const id = '1';
+      const result = controller.findOne(id);
+      expect(result).toBe(`This action returns #${id} coffee`);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all coffees', () => {
+      const result = controller.findAll();
+      expect(result).toBe('This action returns all coffees');
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new coffee', () => {
+      const body = { name: 'Test Coffee' };
+      const result = controller.create(body);
+      expect(result).toEqual(body);
+    });
+  });
+
+  describe('update', () => {
+    it('should update the coffee with the specified id', () => {
+      const id = '1';
+      const body = { name: 'Updated Coffee' };
+      const result = controller.update(id, body);
+      expect(result).toBe(`This action updates #${id} coffee`);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove the coffee with the specified id', () => {
+      const id = '1';
+      const result = controller.remove(id);
+      expect(result).toBe(`This action removes #${id} coffee`);
+    });
   });
 });
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +10,20 @@ export class CoffeesController {
   @Get('')
   public findAll() {
     return 'This action returns all coffees';
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
