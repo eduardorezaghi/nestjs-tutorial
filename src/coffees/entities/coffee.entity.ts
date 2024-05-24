d6fee2d184866276e2e92d948a0d18cd5bf90a72
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-05-24 09:23:02 -0300

    Add TypeORM with Postgres integration

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,8 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('coffees')
 class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @Column('json', { default: 0, nullable: true })
+  flavors?: string[];
 }
 
 export { Coffee };
